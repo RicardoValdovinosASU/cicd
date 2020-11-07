74afc96d87c2be2fcecaa3a42ed948dbe18a4454
Ricardo Valdovinos <rvaldov1@asu.edu>
2020-11-07 07:13:18 -0700

    making changes for jenkins

--- backend/docker-build-run.sh
+++ backend/docker-build-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo docker stop spring-boot-test
+docker stop spring-boot-test
 ./mvnw package #&& java -jar target/test-0.0.1-SNAPSHOT.jar # The command after the && runs the jar file
-sudo docker rm spring-boot-test
-sudo docker build -t backend-spring-boot-test .
-sudo docker run -p 8081:8080 -d --name=spring-boot-test backend-spring-boot-test:latest
+docker rm spring-boot-test
+docker build -t backend-spring-boot-test .
+docker run -p 8081:8080 -d --name=spring-boot-test backend-spring-boot-test:latest
--- frontend/docker-build-run.sh
+++ frontend/docker-build-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo docker stop frontend-test
-sudo docker rm frontend-test
-sudo docker build -t frontend-test .
-sudo docker run -p 8082:3000 -d --name=frontend-test frontend-test
+docker stop frontend-test
+docker rm frontend-test
+docker build -t frontend-test .
+docker run -p 8082:3000 -d --name=frontend-test frontend-test
